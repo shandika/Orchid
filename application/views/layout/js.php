710739125b2eb6e98658469e05f58a566d22c55e
shandika <36485686+shandika@users.noreply.github.com>
2020-04-13 13:04:30 +0700

    Update js.php

--- application/views/layout/js.php
+++ application/views/layout/js.php
@@ -2,7 +2,7 @@
 <script src="<?php echo base_url()?>assets/vendors/popper.js/dist/umd/popper.min.js"></script>
 <script src="<?php echo base_url()?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url()?>assets/assets/js/main.js"></script>
-
+<script src="<?php echo base_url()?>assets/plugins/sweetalerts/sweetalert2.all.min.js"></script>
 
 <script src="<?php echo base_url()?>assets/vendors/chart.js/dist/Chart.bundle.min.js"></script>
 <script src="<?php echo base_url()?>assets/assets/js/dashboard.js"></script>
@@ -28,4 +28,54 @@
             normalizeFunction: 'polynomial'
         });
     })(jQuery);
+</script>
+<?php if($this->session->flashdata('msg')=='success-login'):?>
+        <script type="text/javascript">
+                Swal.fire({
+                  title: 'Terimakasih',
+                  text: 'Selamat Datang <?php echo $this->session->userdata('nama');?>.',
+                  type: 'success'
+              });
+        </script>
+    <?php else:?>
+
+    <?php endif;?>
+
+<script>
+    $('.tombol-logout').on('click', function (e) {
+
+            e.preventDefault();
+            const href = $(this).attr('href');
+
+            Swal.fire({
+            title: 'Anda yakin?',
+            text: "Anda Akan Logout !",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Logout!'
+            }).then((result) => {
+            if(result.value == true) {
+                document.location.href = href;
+            }
+        })
+    });
+</script>
+<script>
+const flashData = $('.flash-data').data('flashdata');
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+if (flashData) {
+  Toast.fire({
+    title: 'Peringatan',
+    text: flashData,
+    type: 'warning'
+  });
+}
 </script>
