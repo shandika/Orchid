d08b0b0d6d7190d0356d191029c4d9b6eec91ac6
shandika <36485686+shandika@users.noreply.github.com>
2020-04-14 14:46:17 +0700

    Update css.php

--- application/views/layout/css.php
+++ application/views/layout/css.php
@@ -7,8 +7,17 @@
 <link rel="stylesheet" href="<?php echo base_url()?>assets/vendors/flag-icon-css/css/flag-icon.min.css">
 <link rel="stylesheet" href="<?php echo base_url()?>assets/vendors/selectFX/css/cs-skin-elastic.css">
 <link rel="stylesheet" href="<?php echo base_url()?>assets/vendors/jqvmap/dist/jqvmap.min.css">
-
+<link rel="stylesheet" href="<?php echo base_url()?>assets/dist/css/smart_wizard.min.css" type="text/css">
+<link rel="stylesheet" href="<?php echo base_url()?>assets/dist/css/smart_wizard_theme_arrows.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()?>assets/dist/css/smart_wizard_theme_circles.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()?>assets/dist/css/smart_wizard_theme_dots.min.css" rel="stylesheet" type="text/css" />
 
 <link rel="stylesheet" href="<?php echo base_url()?>assets/assets/css/style.css">
+<!-- Datatables -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">
 
+
+<!-- Toast -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/plugins/toast/jquery.toast.min.css'?>"/>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
