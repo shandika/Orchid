2fcfe97faaa06fb119875f13be5f3c946c1fad3f
shandika <36485686+shandika@users.noreply.github.com>
2020-04-28 19:22:34 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,3 +1,7 @@
+<?php 
+    $query=$this->db->query("SELECT ID_dp, ID_invoice_dp, nominal_angsuran_dp, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.status = 1 AND angsuran_dp.sisa_angsuran='0' ORDER BY angsuran_dp.ID_dp DESC LIMIT 1");
+    $jum_pesan=$query->num_rows();
+?>
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
 
@@ -11,31 +15,6 @@
 
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <?php if ($this->session->userdata('level') == 2) { ?>
-                    <li class="">
-                        <a href="<?php echo base_url('Home/pm'); ?>"> <i class="menu-icon fa fa-dashboard"></i>Poject</a>
-                    </li>
-                <?php } ?>
-                <?php if ($this->session->userdata('level') == 4) { ?>
-                    <li class="">
-                        <a href="<?php echo base_url('Keuangan/journal'); ?>"> <i class="menu-icon fa fa-id-badge"></i>Journal</a>
-                    </li>
-                <?php } ?>
-                <?php if ($this->session->userdata('level') == 2) { ?>
-                    <li class="">
-                        <a href="<?php echo base_url('pm/pr'); ?>"> <i class="menu-icon fa fa-id-badge"></i>Purchasing Request </a>
-                    </li>
-                <?php } ?>
-                <?php if ($this->session->userdata('level') == 4) { ?>
-                    <li class="">
-                        <a href=" <?php echo base_url('Keuangan/index'); ?>"> <i class="menu-icon fa fa-id-badge"></i>General Ledger</a>
-                    </li>
-                <?php } ?>
-                <?php if ($this->session->userdata('level') == 4) { ?>
-                    <li class="">
-                        <a href=" <?php echo base_url('Keuangan/angsuran'); ?>"> <i class="menu-icon fa fa-id-badge"></i>Bayar Angsuran</a>
-                    </li>
-                <?php } ?>
                 <?php if ($this->session->userdata('level') == 1) { ?>
                     <li class="">
                         <a href="index.html"> <i class="menu-icon fa fa-id-badge"></i>PREORDER BARANG </a>
@@ -49,6 +28,16 @@
                 <?php if ($this->session->userdata('level') == 1) { ?>
                     <li class="">
                         <a href="index.html"> <i class="menu-icon fa fa-id-badge"></i>BEAUTY CONTEST</a>
+                    </li>
+                <?php } ?>
+                <?php if ($this->session->userdata('level') == 2) { ?>
+                    <li class="">
+                        <a href="<?php echo base_url('Home/pm'); ?>"> <i class="menu-icon fa fa-tags"></i>Poject</a>
+                    </li>
+                <?php } ?>
+                <?php if ($this->session->userdata('level') == 2) { ?>
+                    <li class="">
+                        <a href="<?php echo base_url('pm/pr'); ?>"> <i class="menu-icon fa fa-money"></i>Purchasing Request </a>
                     </li>
                 <?php } ?>
                 <?php if ($this->session->userdata('level') == 3) { ?>
@@ -66,7 +55,47 @@
                         <a href="<?php echo base_url('Marketing/upload'); ?>"> <i class="menu-icon fa fa-id-badge"></i>LAPORAN</a>
                     </li>
                 <?php } ?>
+                <?php if ($this->session->userdata('level') == 4) { ?>
+                    <li class="">
+                        <a href="<?php echo base_url('Keuangan/journal'); ?>"> <i class="menu-icon fa fa-id-badge"></i>Journal</a>
+                    </li>
+                <?php } ?>
+                <?php if ($this->session->userdata('level') == 4) { ?>
+                    <li class="">
+                        <a href=" <?php echo base_url('Keuangan/index'); ?>"> <i class="menu-icon fa fa-id-badge"></i>General Ledger</a>
+                    </li>
+                <?php } ?>
+                <?php if ($this->session->userdata('level') == 4) { ?>
+                    <li class="">
+                        <a href=" <?php echo base_url('Keuangan/angsuran'); ?>"> <i class="menu-icon fa fa-id-badge"></i>Bayar Angsuran</a>
+                    </li>
+                <?php } ?>
             </ul>
-        </div><!-- /.navbar-collapse -->
+        </div><!-- /.navbar-collapse --><br><br><br><br><br><br><br>
+        <?php if ($this->session->userdata('level') == 2) { ?>
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">
+                <?php
+                    if($jum_pesan > 0){
+                        echo "Pemberitahuan";
+                    }else{
+                        echo "Tidak Ada Pemberitahuan</p>";
+                    }
+                ?>
+                <small><span class="badge badge-success float-right mt-1">Notif</span></small></strong>
+            </div>
+            <?php 
+                $inbox=$this->db->query("SELECT ID_dp, ID_invoice_dp, nominal_angsuran_dp, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.status = 1 AND angsuran_dp.sisa_angsuran='0' ORDER BY angsuran_dp.ID_dp DESC LIMIT 1");
+                foreach ($inbox->result_array() as $in) :
+                    $nama=$in['nama'];
+                    
+            ?>
+            <div class="card-body">
+            <p class="card-text">Unit Atas Nama <?php echo $nama?> Siap Dibangun</p>
+            </div>
+            <?php endforeach;?>
+        </div>
+        <?php } ?>
     </nav>
 </aside><!-- /#left-panel -->
