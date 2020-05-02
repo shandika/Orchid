8566323ada5d78db94f986daf8b6360f73347e99
Octavian <octaviansaja2@gmail.com>
2020-05-02 13:27:28 +0700

    Update Purchasing Request
    sudah fungsi seluruhnya, tinggal lanjut ke Purchasing

--- application/views/pm/pr.php
+++ application/views/pm/pr.php
@@ -34,7 +34,7 @@
         <label for="exampleFormControlInput1">Jumlah Barang</label>
         <input type="text" class="form-control" id="jumlah_barang_pr" name="jumlah_barang_pr" placeholder="">
     </div>
-    <div class="form-group col-4">
+    <div class="form-group col-3">
         <label for="exampleFormControlInput1">Nama Supplier</label>
         <input type="text" class="form-control" id="nama_supplier_pr" name="nama_supplier_pr" placeholder="">
     </div>
@@ -47,17 +47,25 @@
     </div>
     <div class="form-group col-3">
         <label for="exampleFormControlInput1">Lama Bayar</label>
-        <select id="lama_bayar_pr" name="lama_bayar_pr" class="form-control">
+        <select id="lama_bayar_pr" name="lama_bayar_pr" class="form-control" disabled>
             <option selected value="0">Pilih Jumlah Bulan</option>
-            <option value="1">3</option>
+            <option value="3">3</option>
             <option value="6">6</option>
             <option value="9">9</option>
             <option value="12">12</option>
         </select>
     </div>
-    <div class="form-group col-3">
-        <label for="exampleFormControlInput1">Waktu Tunggu</label>
-        <input type="text" class="form-control" id="waktu_tunggu_pr" name="waktu_tunggu_pr" placeholder="Bulanan">
+    <div class="form-group col-2">
+        <label for="exampleFormControlInput1">Waktu tunggu</label>
+        <input type="text" class="form-control" id="waktu_tunggu_pr" name="waktu_tunggu_pr" placeholder="">
+    </div>
+    <div class="form-group col-2">
+        <label for="exampleFormControlInput1">Satuan</label>
+        <select id="satuan_waktu_tunggu_pr" name="satuan_waktu_tunggu_pr" class="form-control">
+            <option value="hari" selected>Hari</option>
+            <option value="bulan">Bulan</option>
+            <option value="tahun">Tahun</option>
+        </select>
     </div>
     <div class="form-group">
         <button type="submit" class="btn btn-success form-control">Simpan</button>
