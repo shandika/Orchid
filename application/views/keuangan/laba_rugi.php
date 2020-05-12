e44f87e5eb9b7a2866756a6de1f32b8160d5aa41
Octavian <octaviansaja2@gmail.com>
2020-05-12 15:56:12 +0700

    mPDF + pdf laba + neraca beres

--- application/views/keuangan/laba_rugi.php
+++ application/views/keuangan/laba_rugi.php
@@ -1,10 +1,13 @@
-<div class="form-group">
-    <label for="formGroupExampleInput">Pilih Project</label>
-    <select class="form-control" id="project_LR" name="project_LR" onchange="loadlr()">
-        <option selected>Pilih Project</option>
-        <?php foreach ($query as $key) : ?>
-            <option value="<?= $key->nama_gl; ?>"><?= $key->ID_project; ?> / <?= $key->nama; ?></option>
-        <?php endforeach; ?>
-    </select>
-</div>
-<div id="lr"></div>
+<form action="<?= base_url('Keuangan/cetak_laba') ?>" method="POST">
+    <div class="form-group">
+        <label for="formGroupExampleInput">Pilih Project</label>
+        <select class="form-control" id="project_LR" name="project_LR" onchange="loadlr()">
+            <option selected>Pilih Project</option>
+            <?php foreach ($query as $key) : ?>
+                <option value="<?= $key->nama_gl; ?>"><?= $key->ID_project; ?> / <?= $key->nama; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <div id="lr"></div>
+    <button type="submit" class="btn btn-warning col-12"><i class="menu-icon fa fa-file-pdf-o"></i> Export to PDF</button>
+</form>
