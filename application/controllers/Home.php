09d3e905b2a439cca3d0265fe0043a724230234d
shandika <36485686+shandika@users.noreply.github.com>
2020-06-02 00:00:48 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -106,7 +106,7 @@ class Home extends CI_Controller
 	}
 	public function notif_keuangan() {
 		// Angsuran DP
-		$query = $this->db->query("SELECT ID_dp, angsuran_ke, ID_invoice_dp, nominal_angsuran_dp, email, angsuran_dp.date, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_dp.status =0")->result();
+		$query = $this->db->query("SELECT ID_dp, angsuran_ke, ID_invoice_dp, nominal_angsuran_dp, email, angsuran_dp.date, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_dp.status =0 AND angsuran_dp.status_email=0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
@@ -115,8 +115,9 @@ class Home extends CI_Controller
                     
                     <form action="'.base_url('Home/send').'" method="POST">
                         <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input name="id_dp" type="hidden" value="'.$d->ID_dp.'">
                         <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
-	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_dp, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan Angsuran DP tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_dp, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
                         <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
                         </button>
                     </form>   
@@ -127,7 +128,7 @@ class Home extends CI_Controller
 		echo $output;
 
 		// Angsuran Bulanan
-		$query = $this->db->query("SELECT ID_angsuran_bulanan, angsuran_ke, ID_invoice_angsuran_bulanan, email, nominal_angsuran_bulanan, angsuran_bulanan.date, angsuran_bulanan.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_bulanan ON customer.no_ktp = angsuran_bulanan.no_ktp WHERE angsuran_bulanan.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_bulanan.status =0")->result();
+		$query = $this->db->query("SELECT ID_angsuran_bulanan, angsuran_ke, ID_invoice_angsuran_bulanan, email, nominal_angsuran_bulanan, angsuran_bulanan.date, angsuran_bulanan.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_bulanan ON customer.no_ktp = angsuran_bulanan.no_ktp WHERE angsuran_bulanan.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_bulanan.status =0 AND angsuran_bulanan.status_email=0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
@@ -135,8 +136,9 @@ class Home extends CI_Controller
                     <p class="card-text">7 Hari Sebelum Jatuh Tempo Pembayaran Angsuran Bulanan Ke-'.$d->angsuran_ke.' Atas Nama '.$d->nama.'</p>
                     <form action="'.base_url('Home/send').'" method="POST">
                         <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input name="id_bulan" type="hidden" value="'.$d->ID_angsuran_bulanan.'">
                         <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
-	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_bulanan, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan Angsuran Bulanan tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_bulanan, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
                         <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
                         </button>
                     </form>   
@@ -147,7 +149,7 @@ class Home extends CI_Controller
 		echo $output;
 
 		// Angsuran Injek
-		$query = $this->db->query("SELECT ID_injek, angsuran_ke, ID_invoice_injek, nominal_injek, email, angsuran_injek.date, angsuran_injek.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_injek ON customer.no_ktp = angsuran_injek.no_ktp WHERE angsuran_injek.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_injek.status =0")->result();
+		$query = $this->db->query("SELECT ID_injek, angsuran_ke, ID_invoice_injek, nominal_injek, email, angsuran_injek.date, angsuran_injek.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_injek ON customer.no_ktp = angsuran_injek.no_ktp WHERE angsuran_injek.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_injek.status =0 AND angsuran_injek.status_email=0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
@@ -155,8 +157,9 @@ class Home extends CI_Controller
                     <p class="card-text">7 Hari Sebelum Jatuh Tempo Pembayaran Angsuran Injek Ke-'.$d->angsuran_ke.' Atas Nama '.$d->nama.'</p>
                     <form action="'.base_url('Home/send').'" method="POST">
                         <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input name="id_injek" type="hidden" value="'.$d->ID_injek.'">
                         <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
-	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_injek, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan Angsuran Injek tanggal '.$d->date.' sebesar '.number_format($d->nominal_injek, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
                         <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
                         </button>
                     </form>   
@@ -178,7 +181,12 @@ class Home extends CI_Controller
 	      'subjek'=>$subjek,
 	      'content'=>$content
 	    );
-
+	    $id_dp = $this->input->post('id_dp');
+	    $id_bulan = $this->input->post('id_bulan');
+	    $id_injek = $this->input->post('id_injek');
+	    $this->keuangan->updateEmailAngsuranDP($id_dp);
+	    $this->keuangan->updateEmailAngsuranBulanan($id_bulan);
+	    $this->keuangan->updateEmailAngsuranInjek($id_injek);
 	    $send = $this->mailer->send($sendmail);
 	   	if ($send) {
 	   		echo $this->session->set_flashdata('msg', 'success-email');
