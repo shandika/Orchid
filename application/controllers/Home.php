1e3d1b3faac063220a7bf8cab103c372dd23042e
shandika <36485686+shandika@users.noreply.github.com>
2020-06-01 23:33:11 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -28,8 +28,10 @@ class Home extends CI_Controller
 				redirect('Home/purchasing', 'refresh');
 			} elseif ($level == 2) {
 				redirect('Home/pm', 'refresh');
-			} else {
+			} elseif($level==3) {
 				redirect('Home/marketing', 'refresh');
+			}else {
+				redirect('Home/keuangan', 'refresh');
 			}
 		} else {
 			$title = 'Home';
@@ -104,43 +106,100 @@ class Home extends CI_Controller
 	}
 	public function notif_keuangan() {
 		// Angsuran DP
-		$query = $this->db->query("SELECT ID_dp, angsuran_ke, ID_invoice_dp, nominal_angsuran_dp, angsuran_dp.date, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_dp.status =0")->result();
+		$query = $this->db->query("SELECT ID_dp, angsuran_ke, ID_invoice_dp, nominal_angsuran_dp, email, angsuran_dp.date, angsuran_dp.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_dp ON customer.no_ktp = angsuran_dp.no_ktp WHERE angsuran_dp.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_dp.status =0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
 				<div class="card-body">
                     <p class="card-text">7 Hari Sebelum Jatuh Tempo Pembayaran Angsuran DP Ke-'.$d->angsuran_ke.' Atas Nama '.$d->nama.'</p>
-                    <hr />
+                    
+                    <form action="'.base_url('Home/send').'" method="POST">
+                        <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_dp, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+                        <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
+                        </button>
+                    </form>   
                 </div>
+                <hr />
 			';
 		}
 		echo $output;
 
 		// Angsuran Bulanan
-		$query = $this->db->query("SELECT ID_angsuran_bulanan, angsuran_ke, ID_invoice_angsuran_bulanan, nominal_angsuran_bulanan, angsuran_bulanan.date, angsuran_bulanan.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_bulanan ON customer.no_ktp = angsuran_bulanan.no_ktp WHERE angsuran_bulanan.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_bulanan.status =0")->result();
+		$query = $this->db->query("SELECT ID_angsuran_bulanan, angsuran_ke, ID_invoice_angsuran_bulanan, email, nominal_angsuran_bulanan, angsuran_bulanan.date, angsuran_bulanan.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_bulanan ON customer.no_ktp = angsuran_bulanan.no_ktp WHERE angsuran_bulanan.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_bulanan.status =0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
 				<div class="card-body">
                     <p class="card-text">7 Hari Sebelum Jatuh Tempo Pembayaran Angsuran Bulanan Ke-'.$d->angsuran_ke.' Atas Nama '.$d->nama.'</p>
-                    <hr />
+                    <form action="'.base_url('Home/send').'" method="POST">
+                        <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_angsuran_bulanan, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+                        <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
+                        </button>
+                    </form>   
                 </div>
+                <hr />
 			';
 		}
 		echo $output;
 
 		// Angsuran Injek
-		$query = $this->db->query("SELECT ID_injek, angsuran_ke, ID_invoice_injek, nominal_injek, angsuran_injek.date, angsuran_injek.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_injek ON customer.no_ktp = angsuran_injek.no_ktp WHERE angsuran_injek.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_injek.status =0")->result();
+		$query = $this->db->query("SELECT ID_injek, angsuran_ke, ID_invoice_injek, nominal_injek, email, angsuran_injek.date, angsuran_injek.status, customer.nama, customer.no_ktp FROM customer JOIN angsuran_injek ON customer.no_ktp = angsuran_injek.no_ktp WHERE angsuran_injek.date = date_add(curdate(),INTERVAL 7 DAY) AND angsuran_injek.status =0")->result();
 		$output = '';
 		foreach ($query as $d) {
 			$output .='
 				<div class="card-body">
                     <p class="card-text">7 Hari Sebelum Jatuh Tempo Pembayaran Angsuran Injek Ke-'.$d->angsuran_ke.' Atas Nama '.$d->nama.'</p>
-                    <hr />
+                    <form action="'.base_url('Home/send').'" method="POST">
+                        <input name="email_penerima" type="hidden" value="'.$d->email.'">
+                        <input type="hidden" name="pesan" value="Yang Terhormat Bpk/Ibu '.$d->nama.',
+	    Terima kasih atas kepercayaan memilih Royal Orchid Syariah sebagai pilihan untuk keluarga Anda.<br><br>Berikut ini kami informasikan tagihan tanggal '.$d->date.' sebesar '.number_format($d->nominal_injek, 2, ',', '.').'. Silahkan abaikan pemberitahuan ini apabila sudah melakukan pembayaran.<br><br>Pembayaran tagihan Royal Orchid Syariah dapat dilakukan dengan, melalui cara berikut :">
+                        <button title="Kirim Email" onclick="return confirm ("Anda yakin ingin kirim email ?")" class="btn btn-sm btn-success float-right"><i class="fa ti-email"></i>
+                        </button>
+                    </form>   
                 </div>
+                <hr />
 			';
 		}
 		echo $output;
+	}
+	public function send(){
+	    $this->load->library('mailer');
+
+	    $email_penerima = $this->input->post('email_penerima');
+	    $subjek = "Pemberitahuan Pembayaran Angsuran";
+	    $pesan = $this->input->post('pesan');;
+	    $content = $this->load->view('content', array('pesan'=>$pesan), true);
+	    $sendmail = array(
+	      'email_penerima'=>$email_penerima,
+	      'subjek'=>$subjek,
+	      'content'=>$content
+	    );
+
+	    $send = $this->mailer->send($sendmail);
+	   	if ($send) {
+	   		echo $this->session->set_flashdata('msg', 'success-email');
+	        $title = 'Keuangan - General Ledger';
+	        $data = array(
+	            'title' => $title,
+	            'query' => $this->keuangan->tampilDataGL(),
+	            'query2' => $this->db->get('project')->result(),
+	        );
+	        $this->template->load('layout/template_v', 'keuangan/general_ledger', $data);
+	   	}else{
+	   		echo $this->session->set_flashdata('msg', 'error-email');
+	        $title = 'Keuangan - General Ledger';
+	        $data = array(
+	            'title' => $title,
+	            'query' => $this->keuangan->tampilDataGL(),
+	            'query2' => $this->db->get('project')->result(),
+	        );
+	        $this->template->load('layout/template_v', 'keuangan/general_ledger', $data);
+	   	}
+     
 	}
 	public function jum_notif() {
 		$result = $this->pm->jum_msg();
